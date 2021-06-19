e2f8209152e45d54ddf4d8edd5d6c5ef978ab0bf
Qilin <qilin.cheng@foxmail.com>
2021-06-20 00:44:40 +0800

    shaders

---
+++ shaders/shaderc.sh
@@ -0,0 +1,6 @@
+/Users/qilin/Documents/SDK/VulkanSDK/1.2.176.1/macOS/bin/glslangValidator \
+    -V shader.vert.glsl -o shader.vert.spv
+
+/Users/qilin/Documents/SDK/VulkanSDK/1.2.176.1/macOS/bin/glslangValidator \
+    -V shader.frag.glsl -o shader.frag.spv
+    
